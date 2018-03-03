ae7069cafb85358d31100d547b3f43b064e6ee7f
Usman Rafi <usman.rafi.96@gmail.com>
2018-03-03 11:32:23 +0500

    Added main file

--- src/UniversalCinemas.py
+++ src/UniversalCinemas.py
@@ -1,12 +1,21 @@
 import bs4 as bs
 import urllib.request
 import re
+import json
 
 src = urllib.request.urlopen('https://universalcinemas.com/').read()
 soup = bs.BeautifulSoup(src, "lxml")
 
 foo = soup.find_all('script')
 foo = foo[8]
+foo = str(foo)
+
+jd = json.dumps(foo[foo.index("{"):])
+jl = json.loads(jd)
+print (jl)
+
+
+#print(re.split(r"movieData = ",foo.text))
 
 # movies = soup.find("div", {"id": "moviePreviews"})
 
