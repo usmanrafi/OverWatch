f09736ebd048875938d925eec328c852207751fd
Usman Rafi <usman.rafi.96@gmail.com>
2018-03-03 12:23:14 +0500

    Cleaned JSON output for Universal Cinemas

--- src/UniversalCinemas.py
+++ src/UniversalCinemas.py
@@ -10,7 +10,27 @@ foo = soup.find_all('script')
 foo = foo[8]
 foo = str(foo)
 
-jd = json.dumps(foo[foo.index("{"):])
+foo = foo[foo.index("{"):]
+foo = foo[:foo.index("movieDataByReleaseDate")]
+
+commaIndex = foo[:foo.rfind("'2018-")].rfind(",")
+bar = foo[:commaIndex]
+while(commaIndex != -1):
+	foo = foo[:commaIndex] + foo[commaIndex+1:]
+	commaIndex = bar.rfind(",")
+	foo = foo[:commaIndex] + foo[commaIndex+1:]
+	commaIndex = bar[:bar.rfind("'2018-")].rfind(",")
+	bar = foo[:commaIndex]
+
+
+for i in range(0,3):
+	foo = foo[:foo.rindex(",")] + foo[foo.rindex(",")+1:]
+
+while(foo.find("/*will have to change nowPlaying to have separate dates everywhere */") != -1):
+	foo = foo.replace("/*will have to change nowPlaying to have separate dates everywhere */", "")
+
+
+jd = json.dumps(foo)
 jl = json.loads(jd)
 print (jl)
 
